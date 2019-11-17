4c2af5c805b9c6374bcaa5b896a0d031040fa3e6
thierry-khalife <55872793+thierry-khalife@users.noreply.github.com>
2019-11-17 17:09:48 +0100

    Add files via upload

--- profil.php
+++ profil.php
@@ -57,31 +57,82 @@
                 ?>
 
                 <form class="form_profil" action="profil.php" method="post">
-
                     <label> Login </label>
                     <input type="text" name="login" value=<?php echo $resultat['login']; ?> />
-
                     <label> Prénom </label>
                     <input type="text" name="prenom" value=<?php echo $resultat['prenom']; ?> />
-
                     <label> Nom </label>
                     <input type="text" name="nom" value=<?php echo $resultat['nom']; ?> />
-
-                   
-
                     <input id="prodId" name="ID" type="hidden" value=<?php echo $resultat['id']; ?> />
-
                     <input class="mybutton" type="submit" name="modifier" value="Modifier" />
-
                 </form>
-                <br>
-                    <form class="form_profil" action="profil.php" method="post">
-                    <label> Modify Password </label>
+
+                <?php 
+                    if (isset($_POST['modifier']) ) 
+                    {
+                         $login = $_POST["login"];
+                         $prenom = $_POST["prenom"];
+                         $nom = $_POST["nom"];
+                         $req = "SELECT login FROM utilisateurs WHERE login = '$login'";
+                         $req3 = mysqli_query($connexion, $req);
+                         $veriflog = mysqli_fetch_all($req3);
+                         if(!empty($veriflog))
+                            {
+                               echo "Login deja utilisé, requete refusé.<br>";
+                            }
+                        if(empty($veriflog))
+                            {
+                                $updatelog = "UPDATE utilisateurs SET login ='" . $_POST['login'] . "' WHERE id = '" . $resultat['id'] . "'";
+                                $querylog = mysqli_query($connexion, $updatelog); # Execution de la requête;
+                                $_SESSION['login']=$_POST['login'];
+                                header("Location:profil.php");
+                            }
+                        if($resultat['prenom'] != $prenom )
+                            {
+                                $updateprenom = "UPDATE utilisateurs SET prenom ='" . $_POST['prenom'] . "' WHERE id = '" . $resultat['id'] . "'";
+                                $queryprenom = mysqli_query($connexion, $updateprenom); # Execution de la requête;
+                               header("Location:profil.php");
+                            }
+                        if($resultat['nom'] != $nom )
+                            {
+                                $updatenom = "UPDATE utilisateurs SET nom ='" . $_POST['nom'] . "' WHERE id = '" . $resultat['id'] . "'";
+                                $querynom = mysqli_query($connexion, $updatenom); # Execution de la requête;
+                               header("Location:profil.php");
+                            }
+
+       
+                    }
+
+                ?>
+        </section>
+
+         <section class="rightsidebar"> 
+                 <form class="form_profil" action="profil.php" method="post">
+                    <label> New Password </label>
                     <input type="password" name="passwordx" />
+                    <label> Confirm New Password </label>
+                    <input type="password" name="passwordconf" />
                     <input id="prodId" name="ID" type="hidden" value=<?php echo $resultat['id']; ?> />
                     <input class="mybutton" type="submit" name="modifier2" value="Modifier MDP" />
-                    </form>
-        </section>
+                </form>
+
+         <?php 
+                if (isset($_POST['modifier2'])) 
+                    {
+                       if ($_POST["passwordx"] != $_POST["passwordconf"]) 
+                          {
+                            echo "Attention ! Mot de passe différents";
+                          } 
+                       elseif(isset($_POST['passwordx'])){
+                            $pwdx = password_hash($_POST['passwordx'], PASSWORD_BCRYPT, array('cost' => 12));
+                            $updatepwd = "UPDATE utilisateurs SET password = '$pwdx' WHERE id = '" . $resultat['id'] . "'";
+                            $query2 = mysqli_query($connexion, $updatepwd); # Execution de la requête;
+                            header('Location:profil.php');
+                          }
+                    }
+    ?>
+      </section>
+        
     <?php
 
     } 
@@ -90,23 +141,8 @@
         echo "Veuillez vous connecter pour accéder à votre page !";
     }
 
-    if (isset($_POST['modifier'])) 
-    {
-       
-        $update = "UPDATE utilisateurs SET login ='" . $_POST['login'] . "', prenom ='" . $_POST['prenom'] . "', nom ='" . $_POST['nom'] . "' WHERE id = '" . $resultat['id'] . "'";
-        $query3 = mysqli_query($connexion, $update); # Execution de la requête;
-        $_SESSION['login']=$_POST['login'];
-        header('Location:profil.php');
-    }
-    if(isset($_POST['passwordx'])){
-        $pwdx = password_hash($_POST['passwordx'], PASSWORD_BCRYPT, array('cost' => 12));
-        $updatepwd = "UPDATE utilisateurs SET password = '$pwdx' WHERE id = '" . $resultat['id'] . "'";
-        $query2 = mysqli_query($connexion, $updatepwd); # Execution de la requête;
-        header('Location:profil.php');
-    }
-        
     ?>
-
+    
     </main>
 
     <footer>
